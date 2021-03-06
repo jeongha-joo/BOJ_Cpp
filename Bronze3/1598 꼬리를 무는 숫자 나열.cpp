94bd63cc24c233b225e5d16182c46dcbda3e454d
jeongha-joo <64945491+jeongha-joo@users.noreply.github.com>
2021-03-06 22:58:44 +0900

    Update 1598 꼬리를 무는 숫자 나열.cpp

--- Bronze3/1598 꼬리를 무는 숫자 나열.cpp
+++ Bronze3/1598 꼬리를 무는 숫자 나열.cpp
@@ -4,20 +4,17 @@
 using namespace std;
 
 int main() {
-	int a, b, i;
-	int large, small = 0;
+	int a, b;
 	int temp = 0;
 
 	cin >> a >> b;
-	large = max(a, b);
-	small = min(a, b);
-	large--;
-	small--;
+	a--;
+	b--;
 
-	if (a == b || (large - small)%4 == 0) return 0;
-	while ((small/4) != (large/4)) {
+	if (a > b) swap(a, b);
+	while ((a/4) != (b/4)) {
 		temp++;
-		small += 4;
+		a += 4;
 	}
-	cout << temp + abs(large - small);
+	cout << temp + abs(a - b);
 }
